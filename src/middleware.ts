59ec2c2ecb79715836bdb9ffc934513c97c3df27
mlane <mlane@travelpassgroup.com>
2025-04-15 20:21:16 -0600

    Added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { Ratelimit } from '@upstash/ratelimit'
+import { Redis } from '@upstash/redis'
+
+const redis = Redis.fromEnv()
+
+const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(1, '1 m'), // 5 requests per minute
+  analytics: true,
+})
+
+export async function middleware(request: NextRequest) {
+  console.log('checking', request.headers.get('x-forwarded-for'))
+  const ip =
+    request.headers.get('x-forwarded-for')?.split(',')[0] ?? '127.0.0.1'
+
+  const { success } = await ratelimit.limit(ip)
+
+  if (!success) {
+    const key = `limit:violations:${ip}:${new Date()
+      .toISOString()
+      .slice(0, 10)}`
+    console.warn(`[RATE LIMIT BLOCKED] IP: ${ip} - ${new Date().toISOString()}`)
+    const alreadyLogged = await redis.get(key)
+
+    if (!alreadyLogged) await redis.set(key, '1', { ex: 60 * 60 * 24 })
+
+    return new NextResponse(
+      JSON.stringify({
+        reply:
+          "WHOA! I'm emotionally exhausted. You've hit your chaos limit. Try again soon, you storm of feelings!",
+      }),
+      {
+        status: 429,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/api/ask',
+}
